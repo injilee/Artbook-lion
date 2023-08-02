4a77b3caf02d700ebff1e1c438a6c9342e93b6e1
Lee Inji <lij8016@gmail.com>
2023-08-02 11:41:17 +0900

    test : 네이버 도서 검색 api proxy 재수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,21 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
    app.use(
-      createProxyMiddleware('/api', {
+      createProxyMiddleware('/v1/search/book.json', {
          target: 'https://openapi.naver.com',
          changeOrigin: true,
-         pathRewrite: {
-            '^/api/': '/',
-         },
       }),
    );
    app.use(
       createProxyMiddleware('/api', {
          target: 'http://localhost:3000/',
          changeOrigin: true,
-         pathRewrite: {
-            '^/api/': '/',
-         },
       }),
    );
 };
