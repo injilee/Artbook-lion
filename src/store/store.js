b5f22265da564bfed37ed5c0b9a214aae3f80923
Lee Inji <lij8016@gmail.com>
2023-09-05 01:04:36 +0900

    add : redux-persist 추가, 사용자 정보 로컬스토리지에 저장

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
-// state 보관하는 파일
+import storage from 'redux-persist/lib/storage';
 import { configureStore, createSlice } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
 
 export const userSlice = createSlice({
    name: 'user',
@@ -14,19 +15,25 @@ export const userSlice = createSlice({
          state.id = action.payload.id;
          state.token = action.payload.token;
       },
-      // setId: (state, action) => {
-      //    state.id = action.payload;
-      // },
-      // setToken: (state, action) => {
-      //    state.token = action.payload;
-      // },
    },
 });
+
+const persistConfig = {
+   key: 'root',
+   storage,
+   whitelist: ['displayName', 'id', 'token'],
+};
+
+const persistedReducer = persistReducer(persistConfig, userSlice.reducer);
 
 export const { setUser } = userSlice.actions;
 
 export default configureStore({
    reducer: {
-      user: userSlice.reducer,
+      user: persistedReducer,
    },
+   middleware: defaultMiddleware =>
+      defaultMiddleware({
+         serializableCheck: false,
+      }),
 });
